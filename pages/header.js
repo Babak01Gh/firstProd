5db5d1b58aff2a47a3f68019c228fffb889ccf6d
Babak Ghaffari <100711307+Babak01Gh@users.noreply.github.com>
2023-03-11 00:10:50 +0330

    fix bug

--- pages/header.js
+++ pages/header.js
@@ -1,7 +1,7 @@
 const header = `
 <header class="main-head">
     <div class="headNav">
-        <img src="../assets/images/mainLogo.PNG" alt="logo" class="logo">
+        <img src="assets/images/mainLogo.PNG" alt="logo" class="logo">
         <ul class="navList">
            <li><a href="#">صفحه اصلی</a></li>
            <li><a href="#">خدمات ما</a></li>
@@ -16,7 +16,7 @@ const header = `
         </div>
     </div>
     <section class="banner">
-        <img src="../assets/images/banner.jpg" alt="banner" width="100%" height="100%">
+        <img src="assets/images/banner.jpg" alt="banner" width="100%" height="100%">
         <div class="banner--titles">
             <span>ارائه دهنده انواع خدمات</span>
             <h1><strong>دفتر پیشخوان خدمات دولت و بخش عمومی</strong></h1>
--- pages/seeAlso.js
+++ pages/seeAlso.js
@@ -4,7 +4,7 @@ const seeAlso = `
     <div class="swiperSeeAlso">
         <div class="swiper-wrapper">
             <div class="seeAlso--part swiper-slide">
-                <img src="../assets/images/postLogo.jpg" alt="" class="part--img" width="100%">
+                <img src="assets/images/postLogo.jpg" alt="" class="part--img" width="100%">
                 <div class="part--details">
                     <span class="de-head">انجام کلیه خدمات پستی</span>
                     <p class="de-descrip">انجام خدمات مربوط به شرکت پست و مرسولات پستی به بهترین شکل</p>
@@ -12,7 +12,7 @@ const seeAlso = `
                 </div>
             </div>
             <div class="seeAlso--part swiper-slide">
-                <img src="../assets/images/postex.png" alt="" class="part--img" width="100%">
+                <img src="assets/images/postex.png" alt="" class="part--img" width="100%">
                 <div class="part--details">
                     <span class="de-head">پستکس</span>
                     <p class="de-descrip"> ارائه دهنده انواع خدمات پست آنلاین و جمع آوری به صورت درب تا درب</p>
@@ -20,7 +20,7 @@ const seeAlso = `
                 </div>
             </div>
             <div class="seeAlso--part swiper-slide">
-                <img src="../assets/images/rahvar.jpg" alt="" class="part--img" width="100%">
+                <img src="assets/images/rahvar.jpg" alt="" class="part--img" width="100%">
                 <div class="part--details">
                     <span class="de-head"> هلدینگ بزرگ ره وار</span>
                     <p class="de-descrip"> سامانه هوشمند مسافربری بین شهری ره وار جهت سفری ایمن و مقرون بصرفه</p>
@@ -28,7 +28,7 @@ const seeAlso = `
                 </div>
             </div>
             <div class="seeAlso--part swiper-slide">
-                <img src="../assets/images/iranish.jpg" alt="" class="part--img" width="100%">
+                <img src="assets/images/iranish.jpg" alt="" class="part--img" width="100%">
                 <div class="part--details">
                     <span class="de-head">پاساژ مجازی ایرانیش</span>
                     <p class="de-descrip">پاساژ مجازی ایرانیش اولین و تنهاترین مجموعه خدماتی و تجاری در سطح ایران</p>
@@ -36,7 +36,7 @@ const seeAlso = `
                 </div>
             </div>
             <div class="seeAlso--part swiper-slide">
-                <img src="../assets/images/muyrag.jpeg" alt="" class="part--img" width="100%">
+                <img src="assets/images/muyrag.jpeg" alt="" class="part--img" width="100%">
                 <div class="part--details">
                     <span class="de-head">مویرگ | بیمه</span>
                     <p class="de-descrip">کارگزاری رسمی بیمه مستقیم برخط. کسب درآمد پایدار , قانونی و بدون سقف</p>
